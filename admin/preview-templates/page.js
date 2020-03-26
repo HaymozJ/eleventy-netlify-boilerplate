531e16199c5411335bcfaf2826619f1b3d43b83c
Johnny Haymoz <johnny@hemmer.ch>
2020-03-26 12:13:11 +0100

    added custsom cms preview

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -7,24 +7,24 @@ const html = htm.bind(h);
 export default class Page extends React.Component{
     render() {
         const entry = this.props.entry;
-        return html`
-      <main>
-        <div id="carouselExampleSlidesOnly" class="carousel slide headerImage" data-ride="carousel">
-            <div class="carousel-inner">
-              ${this.props.widgetFor("Carousel").map(function (item, index) {
-                return html`
-                  <div class="carousel-item active">
-                      <img src="${item.getIn('data', 'slide')}" alt="" class="d-block w-100" height="500px" />
+        return `
+          <main>
+            <div id="carouselExampleSlidesOnly" class="carousel slide headerImage" data-ride="carousel">
+                <div class="carousel-inner">
+                  ${this.props.widgetFor("Carousel").map(function (item, index) {
+                    return html`
+                      <div class="carousel-item active">
+                          <img src="${item.getIn('data', 'slide')}" alt="" class="d-block w-100" height="500px" />
+                      </div>
+                    `;
+                  })}
+                  <div class="carousel-caption d-none d-md-block">
+                    <h1> ${entry.getIn(["data", "title"], null)}</h1>
                   </div>
-                `;
-              })}
-              <div class="carousel-caption d-none d-md-block">
-                <h1> ${entry.getIn(["data", "title"], null)}</h1>
-              </div>
+                </div>
             </div>
-        </div>
-        ${this.props.widgetFor("body")}
-      </main>
-    `;
+            ${this.props.widgetFor("body")}
+          </main>
+        `
     }
 }
