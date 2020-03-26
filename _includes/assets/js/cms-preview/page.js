202c1aa0d796e1a024121a1777247e573932a8a6
Johnny Haymoz <johnny@hemmer.ch>
2020-03-26 10:34:09 +0100

    added custsom cms preview

--- _includes/assets/js/cms-preview/page.js
+++ _includes/assets/js/cms-preview/page.js
@@ -9,25 +9,25 @@ export default class PostPreview extends React.Component{
         if (image && !image.fileObj) {
             image = window.parent.location.protocol + "//" + window.parent.location.host + image;
         }
-        return
-        {% if headerImage and not Carousel %}
-            <div class="headerImage" style='background-image: url({{ headerImage }} )'>
-                <h1> {{ title }}</h1>
-            </div>
-        {% elif Carousel %}
-            <div id="carouselExampleSlidesOnly" class="carousel slide headerImage" data-ride="carousel">
-                <div class="carousel-inner">
-                    {% for item in Carousel %}
-                        <div class="carousel-item {% if loop.first %} active {% endif %}">
-                            <img class="d-block w-100" src="{{ item.slide }}" alt="Slide{{ loop.index }}" height="500px">
-                        </div>
-                     {% endfor %}
-                    <div class="carousel-caption d-none d-md-block">
-                        <h1> {{ title }}</h1>
-                    </div>
-                < /div>
-            </div>
-        {% endif %}
+        return "<div>Mon TEST</div>"
+        // {% if headerImage and not Carousel %}
+        //     <div class="headerImage" style='background-image: url({{ headerImage }} )'>
+        //         <h1> {{ title }}</h1>
+        //     </div>
+        // {% elif Carousel %}
+        //     <div id="carouselExampleSlidesOnly" class="carousel slide headerImage" data-ride="carousel">
+        //         <div class="carousel-inner">
+        //             {% for item in Carousel %}
+        //                 <div class="carousel-item {% if loop.first %} active {% endif %}">
+        //                     <img class="d-block w-100" src="{{ item.slide }}" alt="Slide{{ loop.index }}" height="500px">
+        //                 </div>
+        //              {% endfor %}
+        //             <div class="carousel-caption d-none d-md-block">
+        //                 <h1> {{ title }}</h1>
+        //             </div>
+        //         < /div>
+        //     </div>
+        // {% endif %}
         ;
     }
 }
