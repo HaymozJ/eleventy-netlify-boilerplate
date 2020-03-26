beaf5735ad8f6bbbdf6e45a1725d1f05d4fb37c6
Johnny Haymoz <johnny@hemmer.ch>
2020-03-26 11:51:30 +0100

    added custsom cms preview

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -7,13 +7,12 @@ const html = htm.bind(h);
 export default class Page extends React.Component{
     render() {
         const entry = this.props.entry;
-
         return html`
       <main>
         <div id="carouselExampleSlidesOnly" class="carousel slide headerImage" data-ride="carousel">
             <div class="carousel-inner">
-                {(entry.getIn(["data", "Carousel"]) || []).map((slide, i) => <div className="carousel-item active" key={i}>
-                    <img src={getAsset(slide.get("slide"))} alt="" className="d-block w-100" height="500px" />
+                {(entry.getIn(["data", "Carousel"]) || []).map((slide, i) => <div class="carousel-item active" key={i}>
+                    <img src={getAsset(slide.get("slide"))} alt="" class="d-block w-100" height="500px" />
                 </div>)}
                 <div class="carousel-caption d-none d-md-block">
                     <h1> ${entry.getIn(["data", "title"], null)}</h1>
