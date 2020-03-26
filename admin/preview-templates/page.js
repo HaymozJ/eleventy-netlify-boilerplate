28b1d6fb4680e633049a0d65dd6cd43fd5a6dafc
Johnny Haymoz <johnny@hemmer.ch>
2020-03-26 10:48:40 +0100

    added custsom cms preview

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -4,18 +4,34 @@ const html = htm.bind(h);
 
 // Preview component for a Page
 const Page = createClass({
-  render() {
-    const entry = this.props.entry;
+    render() {
+        const entry = this.props.entry;
 
-    return html`
+        return html`
       <main>
-      <h1>lol</h1>
+        {% if headerImage and not Carousel %}
+            <div class="headerImage" style='background-image: url({{ headerImage }} )'>
+                <h1> {{ title }}</h1>
+            </div>
+        {% elif Carousel %}
+            <div id="carouselExampleSlidesOnly" class="carousel slide headerImage" data-ride="carousel">
+                <div class="carousel-inner">
+                    {% for item in Carousel %}
+                        <div class="carousel-item {% if loop.first %} active {% endif %}">
+                            <img class="d-block w-100" src="{{ item.slide }}" alt="Slide{{ loop.index }}" height="500px">
+                        </div>
+                    {% endfor %}
+                    <div class="carousel-caption d-none d-md-block">
+                        <h1> {{ title }}</h1>
+                    </div>
+                </div>
+            </div>
+        {% endif %}
         <h1>${entry.getIn(["data", "title"], null)}</h1>
-
         ${this.props.widgetFor("body")}
       </main>
     `;
-  }
+    }
 });
 
 export default Page;
