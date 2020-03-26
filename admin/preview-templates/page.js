5cd5c37f02418383d57c90d0c83356e992619bee
Johnny Haymoz <johnny@hemmer.ch>
2020-03-26 12:19:42 +0100

    added custsom cms preview

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -1,30 +1,20 @@
+
 import htm from "https://unpkg.com/htm?module";
-import React from "react";
 
 const html = htm.bind(h);
 
 // Preview component for a Page
-export default class Page extends React.Component{
-    render() {
-        const entry = this.props.entry;
-        return `
-          <main>
-            <div id="carouselExampleSlidesOnly" class="carousel slide headerImage" data-ride="carousel">
-                <div class="carousel-inner">
-                  ${this.props.widgetFor("Carousel").map(function (item, index) {
-                    return html`
-                      <div class="carousel-item active">
-                          <img src="${item.getIn('data', 'slide')}" alt="" class="d-block w-100" height="500px" />
-                      </div>
-                    `;
-                  })}
-                  <div class="carousel-caption d-none d-md-block">
-                    <h1> ${entry.getIn(["data", "title"], null)}</h1>
-                  </div>
-                </div>
-            </div>
-            ${this.props.widgetFor("body")}
-          </main>
-        `
-    }
-}
+const Page = createClass({
+  render() {
+    const entry = this.props.entry;
+
+    return html`
+      <main>
+        <h1>${entry.getIn(["data", "title"], null)}</h1>
+        ${this.props.widgetFor("body")}
+      </main>
+    `;
+  }
+});
+
+export default Page;
