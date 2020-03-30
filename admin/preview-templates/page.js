10f1bcf230b5967bb72da3bba90ef68260305352
Johnny Haymoz <johnny@hemmer.ch>
2020-03-30 11:29:52 +0200

    Modified backend preview

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -50,6 +50,7 @@ const Page = createClass({
                         return html `
                             <div class="row">
                                 <img src="${getAsset(item.get("imgOnly"))}" width="100%"/>
+                                <p>${this.props.widgetFor("txtOnly")}</p>
                             </div>
                         `;    
                     }):''}
