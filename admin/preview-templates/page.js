6b7c41ef2cdd71b3e544687160829d54fc17e2a7
Johnny Haymoz <johnny@hemmer.ch>
2020-03-31 09:31:25 +0200

    Modified backend preview

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -48,13 +48,13 @@ const Page = createClass({
                     ${entry.getIn(["data","blocks"])!=null?entry.getIn(["data","blocks"]).map((item, index) =>{
                         return html `
                             <div class="row">
-                                ${item.getIn("imgOnly")!=null?() =>{
+                                ${item.getIn("imgOnly")!=null?(() =>{
                                         return html `
                                             <div class="col-md-12">
                                                  <img src="${getAsset(item.get("imgOnly"))}" width="100%"/>
                                             </div>
                                         `
-                                }:''}
+                                }):''}
                                 <div class="col-md-12">
                                     <p>${item.getIn(['txtOnly'])}</p>
                                 </div>
